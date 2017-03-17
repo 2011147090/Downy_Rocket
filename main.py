f321dce9768d492d25b495b58dcce166afc78af2
Nam <n2011147090@gmail.com>
2017-03-17 20:45:27 +0900

    쓸데없는 부분 제

--- main.py
+++ main.py
@@ -11,38 +11,12 @@ my_tag = 'disgusting, baby'
 
 
 srch = flickr.photos.search(tags=my_tag, tag_mode='all', text=my_tag, safe_search=3, sort='relevance', page=2)
+
 for i in srch['photos']['photo']:
     try:
         urls = flickr.photos.getSizes(photo_id=i['id'])
     except:
         continue
-    try:
-        print urls['sizes']['size'][4]['source']
-    except:
-        continue
 
-    # for i in urls['sizes']['size']:
-    #     print i
-    #
-    # print ''
+    print urls['sizes']['size'][1]['source']
 
-
-
-# clus = flickr.tags.getClusters(tag=my_tag)
-# for i in clus['clusters']['cluster']:
-#     print i
-
-
-# rela = flickr.tags.getRelated(tag=my_tag)
-# for i in rela['tags']['tag']:
-#     print i
-
-
-# tags = flickr.tags.getClusterPhotos(tag=my_tag)
-# for i in tags[u'photos'][u'photo']:
-#     try:
-#         urls = flickr.photos.getSizes(photo_id=i['id'])
-#     except:
-#         continue
-#
-#     print urls['sizes']['size'][-1]['source']
