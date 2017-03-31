348b9c899496028d8ea241568f11b9f08e1a6367
Downy <downy7811@gmail.com>
2017-03-31 18:29:50 +0900

    Bulding JSON complete

--- flickr_crawling.py
+++ flickr_crawling.py
@@ -1,20 +1,23 @@
 import requests
 import json
-import sys
 from bs4 import BeautifulSoup
 
 city_adjective = open('data/city.txt', 'r')
 base_url = 'https://www.flickr.com/search/?text={}%20city'
 
+dict = {}
+
 #processing adjective file
 for line in city_adjective.readlines():
     splitted_line = line.strip().split('_')
-    splitted_line = splitted_line[0].split('-')
-    if len(splitted_line) == 1:
-        url = base_url.format(splitted_line[0])
+    noun = splitted_line[-1].strip()
+    adjective_list = []
+    adjective_list = splitted_line[0].split('-')
+    if len(adjective_list) == 1:
+        url = base_url.format(adjective_list[0])
         #print (url)
-    elif len(splitted_line) == 2:
-        url = base_url.format('{}%20{}'.format(splitted_line[0], splitted_line[1]))
+    elif len(adjective_list) == 2:
+        url = base_url.format('{}%20{}'.format(adjective_list[0], adjective_list[1]))
         #print (url)
     else:
         print ("Exception: {}").format(line)
@@ -22,10 +25,18 @@ for line in city_adjective.readlines():
     soup = BeautifulSoup(r.text, 'lxml')
 
     photo_urls = soup.find_all('div', attrs={'class': "view photo-list-photo-view requiredToShowOnServer awake"})
-    print (line)
-    for photo_url in photo_urls:
+    print (line.strip())
+    for i, photo_url in enumerate(photo_urls):
+        photo_id = "{}_{}".format(line.strip().replace('-','_'), i)
         base_photo_url = "c1.staticflickr.com{}.jpg"
         parsed_url = str(photo_url).split('c1.staticflickr.com')[1].split('.jpg')[0]
         photo = base_photo_url.format(parsed_url)
-        print (photo)
-    print ("-"*70)
+        if noun in dict.keys():
+            dict[noun].update({photo_id : {'id': photo_id, 'url':photo, 'adjective':adjective_list, 'noun':noun, 'path':'dataset/{}'.format(photo_id)}})
+        else:
+            dict[noun] = {photo_id: {'id': photo_id, 'url': photo, 'adjective': adjective_list, 'noun': noun, 'path':'dataset/{}'.format(photo_id)}}
+        #print (photo)
+    #print ("-"*70)
+
+with open('photo_profile.json', 'w') as write_json:
+    json.dump(dict, write_json)
